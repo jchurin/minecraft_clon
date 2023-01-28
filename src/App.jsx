a4bb397f6c16bc79464f1556057232f0f4db36f1
Juan Churin <juanchurin@gmail.com>
2023-01-28 15:56:35 -0300

    feat: add player and camera movements

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Sky } from '@react-three/drei'
 import { Physics } from '@react-three/cannon'
 import Ground from './components/ground'
 import Fpv from './components/fpv'
+import Player from './components/player'
 
 function App () {
   return (
@@ -11,6 +12,7 @@ function App () {
       <ambientLight intensity={0.5} />
       <Fpv />
       <Physics>
+        <Player />
         <Ground />
       </Physics>
     </Canvas>
